0644e8b575ade8d3e039326de33f2ba707835307
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-04-22 14:00:44 +0200

    register slider block

--- functions.php
+++ functions.php
@@ -139,20 +139,32 @@ function gup_underscore_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'gup_underscore_scripts' );
 
-/**
- * Slick Slider.
- */
+// Register a slider block.
+add_action('acf/init', 'my_register_blocks');
+function my_register_blocks() {
 
-add_action( 'wp_enqueue_scripts', 'themeprefix_slick_enqueue_scripts_styles' );
-// Enqueue Slick scripts and styles
-function themeprefix_slick_enqueue_scripts_styles() {
+    // check function exists.
+    if( function_exists('acf_register_block_type') ) {
 
-	wp_enqueue_script( 'slickjs', get_stylesheet_directory_uri() . '/js/slick.min.js', array( 'jquery' ), '1.6.0', true );
-	wp_enqueue_script( 'slickjs-init', get_stylesheet_directory_uri(). '/js/slick-init.js', array( 'slickjs' ), '1.6.0', true );
+        // register a testimonial block.
+        acf_register_block_type(array(
+            'name'              => 'slider',
+            'title'             => __('Slider'),
+            'description'       => __('A custom slider block.'),
+            'render_template'   => 'template-parts/blocks/slider/slider.php',
+			'category'          => 'formatting',
+			'icon' 				=> 'images-alt2',
+			'align'				=> 'full',
+			'enqueue_assets' 	=> function(){
+				wp_enqueue_style( 'slick', 'http://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css', array(), '1.8.1' );
+				wp_enqueue_style( 'slick-theme', 'http://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css', array(), '1.8.1' );
+				wp_enqueue_script( 'slick', 'http://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js', array('jquery'), '1.8.1', true );
 
-	wp_enqueue_style( 'slickcss', get_stylesheet_directory_uri() . '/css/slick.css', '1.6.0', 'all');
-	wp_enqueue_style( 'slickcsstheme', get_stylesheet_directory_uri(). '/css/slick-theme.css', '1.6.0', 'all');
-
+				wp_enqueue_style( 'block-slider', get_template_directory_uri() . '/template-parts/blocks/slider/slider.min.css', array(), '1.0.0' );
+				wp_enqueue_script( 'block-slider', get_template_directory_uri() . '/template-parts/blocks/slider/slider.min.js', array(), '1.0.0', true );
+			  },
+        ));
+    }
 }
 
 /**
@@ -186,6 +198,8 @@ if ( defined( 'JETPACK__VERSION' ) ) {
  * Disable WordPress Admin Bar for all users but admins. 
  */
 show_admin_bar(false);
+
+
 
 /** 
  * Add google Fonts. 
