7aef0982b14d51b4fa1568ce47cc3e2c05db9a3e
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-04-28 12:45:21 +0200

    x

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-jQuery.noConflict()
-jQuery(document).ready(function(){
-    (function($){
-	$('#open_menuOverlay').click(function(){
-    	$('#menuOverlay').slideToggle();
-    });
-    })(jQuery);
-});
+function openMenuoverlay() {
+   var x = document.getElementById("menuOverlay");
+   if (x.style.display === "none") {
+     x.style.display = "block";
+   } else {
+     x.style.display = "none";
+   }
+ }
