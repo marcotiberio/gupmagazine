5802e81e424b3f119b44c5342eaa2c3165f82f8e
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-04-28 12:39:59 +0200

    x

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 /* Compatibility Mode */
-jQuery('#menuOverlay').on('click', function() {
-   jQuery("#menuOverlay").toggle();
+jQuery('#closeModal').on('click', function() {
+   jQuery("#modal").hide();
 })
 
-// function openMenuoverlay() {
-//    var x = document.getElementById("menuOverlay");
-//    if (x.style.display === "none") {
-//      x.style.display = "block";
-//    } else {
-//      x.style.display = "none";
-//    }
-//  }
+function openMenuoverlay() {
+   var x = document.getElementById("menuOverlay");
+   if (x.style.display === "none") {
+     x.style.display = "block";
+   } else {
+     x.style.display = "none";
+   }
+ }
