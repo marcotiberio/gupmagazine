c5cdacfcc2cd4ae367474e66aa73849f0faa5f74
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-04-28 14:02:34 +0200

    test

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function openMenuoverlay() {
    var x = document.getElementById("menuOverlay");
    if (x.style.display === "none") {
-     x.style.display = "block";
+     x.style.display = "grid";
    } else {
      x.style.display = "none";
    }
