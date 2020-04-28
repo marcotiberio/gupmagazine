9eb949c8f46549dbe7c0cb93191b85ecce8aa819
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-04-28 12:43:20 +0200

    x

--- js/script.js
+++ js/script.js
@@ -1,18 +1,4 @@
-/* Compatibility Mode */
-jQuery('#closeModal').on('click', function() {
-   jQuery("#modal").hide();
-})
-
-function openMenuoverlay() {
-   var x = document.getElementById("menuOverlay");
-   if (x.style.display === "none") {
-     x.style.display = "block";
-   } else {
-     x.style.display = "none";
-   }
- }
-
- jQuery.noConflict()
+jQuery.noConflict()
 jQuery(document).ready(function(){
     (function($){
 	$('#open_menuOverlay').click(function(){
