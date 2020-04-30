143ee5ba0f170d6301c5a178935c79b1e104d4f1
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-04-30 10:37:36 +0200

    vanilla js overlay

--- js/script.js
+++ js/script.js
@@ -2,19 +2,19 @@ jQuery( document ).ready(function($) {
   alert ('test');
 });
 
-$(document).ready(function(){
-  $("#open_menuOverlay").click(function(){
-    $("#menuOverlay").toggle();
-  });
-});
+// $(document).ready(function(){
+//   $("#open_menuOverlay").click(function(){
+//     $("#menuOverlay").toggle();
+//   });
+// });
 
 
 
-// function openMenuoverlay() {
-//    var x = document.getElementById("menuOverlay");
-//    if (x.style.display === "none") {
-//      x.style.display = "grid";
-//    } else {
-//      x.style.display = "none";
-//    }
-//  }
+function openMenuoverlay() {
+   var x = document.getElementById("menuOverlay");
+   if (x.style.display === "none") {
+     x.style.display = "grid";
+   } else {
+     x.style.display = "none";
+   }
+ }
