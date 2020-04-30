68af51ef3e43feb1ab05bdd5e81a023cc5f35a8c
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-04-30 10:28:51 +0200

    xx

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@ jQuery( document ).ready(function($) {
 
 
 
-// function openMenuoverlay() {
-//    var x = document.getElementById("menuOverlay");
-//    if (x.style.display === "none") {
-//      x.style.display = "grid";
-//    } else {
-//      x.style.display = "none";
-//    }
-//  }
+function openMenuoverlay() {
+   var x = document.getElementById("menuOverlay");
+   if (x.style.display === "none") {
+     x.style.display = "grid";
+   } else {
+     x.style.display = "none";
+   }
+ }
