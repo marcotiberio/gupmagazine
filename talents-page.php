d8d4ecb3ab2db378dc97b45aaa056e57129f9434
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-05-08 15:22:09 +0200

    test

--- talents-page.php
+++ talents-page.php
@@ -4,8 +4,44 @@
 
 <?php get_header(); ?>
 
-	<div id="primary--page-main" class="content-area--page-main">
+<div id="primary--page-main" class="content-area--page-main">
 		<main id="main--page-main" class="site-main--page-main">
+
+		<?php
+		if ( have_posts() ) :
+
+			if ( is_home() && ! is_front_page() ) :
+				?>
+				<header>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
+				<?php
+			endif;
+
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
+
+				/*
+				 * Include the Post-Type-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+				 */
+				get_template_part( 'template-parts/content', get_post_type() );
+
+			endwhile;
+
+			the_posts_navigation();
+
+		else :
+
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif;
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
             
 
         <section class="talents-slider">
@@ -48,9 +84,6 @@
                 </div>
             </main>
 		
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
 
 <?php
 get_sidebar();
