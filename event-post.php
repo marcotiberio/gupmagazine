3797a8cac264fe4208d1914c7496f25aa5277577
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-05-12 15:34:35 +0200

    link

--- event-post.php
+++ event-post.php
@@ -74,7 +74,7 @@
                     $link_title = $link['title'];
                     $link_target = $link['target'] ? $link['target'] : '_blank';
                     ?>
-                    <h4>Artist Website</h4>
+                    <h4>Website</h4>
                     <a class="button" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
 				<?php endif; ?>
             </div>
