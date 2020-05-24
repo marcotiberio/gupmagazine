b8dbff7390fa318a10c25930fc5e47c74ca4cf3f
Marco <marco@defrostudio.eu>
2020-05-24 16:07:30 +0200

    no post subtititle and date

--- latest-posts-articles.php
+++ latest-posts-articles.php
@@ -36,10 +36,10 @@
 						<div>
 						<header class="entry-header">
 							<a href="<?php the_permalink(); ?>"><h3 class="post-title"><?php the_title(); ?></h3></a>
-							<h5 class="post-subtitle">
+							<!-- <h5 class="post-subtitle">
 								<span><?php the_field('post_subtitle'); ?></span> | 
 								<span><?php the_time('m/j/y') ?></span>
-							</h5>
+							</h5> -->
 						</header>
 						<div class="entry-content">
 							<?php the_excerpt(); ?>
