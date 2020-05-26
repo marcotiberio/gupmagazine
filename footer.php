f42eb8aee80cd571bb52daf2996fe60c72861c32
Marco <marco@defrostudio.eu>
2020-05-26 10:58:23 +0200

    test newsletter

--- footer.php
+++ footer.php
@@ -40,7 +40,11 @@
 			<div class="footer-custom-column" id="footer-custom-col3">
 			<h4>NEWSLETTER</h4>
 			<p>Stay up to date on the latest publications, events, articles and open calls</p>
-			<script type="text/javascript" src="//sgwidget.leaderapps.co/js/sendgrid-widget.js"></script><div id="sendgrid-subscription-widget" class="sendgrid-subscription-widget" ><form id="sg-widget" data-token="e2c37163dc95b4351b82ab699ae28b29" onsubmit="return false;"><div class="sg-response" id="sg-response"></div><input id="email" type="email" name="email" placeholder="you@example.com" required><input type="submit" id="sg-submit-btn" value=“Subscribe”></form></div>
+				<!-- NEWSLETTER -->
+				<div class="sendgrid-subscription-widget" data-token="1M5Z249eGJzJ34D5llN3s2KkzNImaU9gZp8ImuJSw1pmhsJvugAYeWJXhtK1aWLO"></div>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://s3.amazonaws.com/subscription-cdn/0.2/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'sendgrid-subscription-widget-js');</script>
+				</div>
+				<!-- NEWSLETTER -->
 			</div>
 			<div class="footer-custom-column" id="footer-custom-col4">
 				<h4>FOLLOW US</h4>
