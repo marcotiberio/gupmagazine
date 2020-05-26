516df0803dc6005de1c4c605d084007aa09fac94
Marco <marco@defrostudio.eu>
2020-05-26 11:02:56 +0200

    >

--- footer.php
+++ footer.php
@@ -45,8 +45,10 @@
 				<div id="sendgrid-subscription-widget" class="sendgrid-subscription-widget" >
 					<form id="sg-widget" data-token="e2c37163dc95b4351b82ab699ae28b29" onsubmit="return false;">
 						<div class="sg-response" id="sg-response"></div>
+						<div>
 						<input id="email" type="email" name="email" placeholder="you@example.com" required>
 						<input type="submit" id="sg-submit-btn" value=“Subscribe”>
+						</div>
 					</form>
 				</div>
 				<!-- NEWSLETTER -->
