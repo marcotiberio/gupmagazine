61223d2d4b7d23dfa97b92d9e50ad42f98f17bf6
Marco <marco@defrostudio.eu>
2020-05-27 13:31:23 +0200

    test

--- article-post.php
+++ article-post.php
@@ -119,7 +119,8 @@ $my_query->the_post();?>
         <a href="<? the_permalink()?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_post_thumbnail( 'thumbnail-list-related' ); ?></a>
         <div class="relatedcontent">
             <h3><a href="<? the_permalink()?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a></h3>
-            <p><?php the_excerpt(); ?></p>
+            <p><?php $summary = get_field('article_text');
+          		echo substr($summary, 0, 300); ?>...</p>
         </div>
     </div>
 <?php
