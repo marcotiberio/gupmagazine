1e4cca8083ccadea20f89609e3f82497d8c1eee1
Marco <marco@defrostudio.eu>
2020-06-25 13:49:21 +0200

    test

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ) ?>">
 	<label>
 		<span class="screen-reader-text"><?php _x( 'Search for:', 'label' )?></span>
-		<input type="search" class="search-field" placeholder="assets/img/close.svg" value="<?php echo get_search_query() ?>" name="s" />
+		<input type="search" class="search-field" placeholder="https://gupmagazine.kinsta.cloud/wp-content/uploads/2020/04/GUP-NEW-logo-2019.1.png" value="<?php echo get_search_query() ?>" name="s" />
 	</label>
 </form>
