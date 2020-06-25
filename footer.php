54e098de931d7006d80ce6876dafc2237565bf0d
Marco <marco@defrostudio.eu>
2020-06-25 11:58:40 +0200

    test newsletter

--- footer.php
+++ footer.php
@@ -37,18 +37,17 @@
 			</div>
 			<div class="footer-custom-column" id="footer-custom-col3">
 			<h4>NEWSLETTER</h4>
-			<p>Stay updadet on our latest publications, events, articles and open calls</p>
+			<p>Stay updated on our latest publications, events, articles and open calls</p>
 				<!-- NEWSLETTER -->
-				<script type="text/javascript" src="//sgwidget.leaderapps.co/js/sendgrid-widget.js"></script>
-				<div id="sendgrid-subscription-widget" class="sendgrid-subscription-widget" >
-					<form id="sg-widget" data-token="b6hYR_PXTFyyzIajdr4JTw" onsubmit="return false;">
-						<div class="newsletterInput">
-							<input id="email" type="email" name="email" placeholder="you@example.com" required>
-							<input type="submit" id="sg-submit-btn" value=“Subscribe”>
-						</div>
-						<div class="sg-response" id="sg-response"></div>
-					</form>
-				</div>
+				<form style="border:1px solid #ccc;padding:3px;text-align:center;" 
+						action="https://tinyletter.com/gupmagazine" 
+						method="post" 
+						target="popupwindow" 
+						onsubmit="window.open('https://tinyletter.com/gupmagazine', 'popupwindow', 'scrollbars=yes,width=800,height=600');return true">
+				<p><label for="tlemail">Enter your email address</label></p>
+				<p><input type="text" style="width:140px" name="email" id="tlemail" /></p>
+				<input type="hidden" value="1" name="embed"/>
+				<input type="submit" value="Subscribe" /><p>
 				<!-- NEWSLETTER -->
 			</div>
 			<div class="footer-custom-column" id="footer-custom-col4">
