cbb92dfad06c358b2a4d4136d35c023c2b77cbe7
Marco <marco@defrostudio.eu>
2020-06-25 13:03:22 +0200

    test search

--- functions.php
+++ functions.php
@@ -228,17 +228,17 @@ function custom_add_google_fonts() {
 // }
 // add_filter( 'get_search_form', 'wpforo_search_form' );
 
-function html5_search_form( $form ) { 
-	$form = '<section class="search">
-				<form role="search" method="get" id="search-form" action="' . home_url( '/' ) . '" >
-   					<label class="screen-reader-text" for="s">' . __('',  'domain') . '</label>
-					<input type="search" value="' . get_search_query() . '" name="s" id="s" placeholder="&#x1F50D;"/>
-				</form>
-			</section>';
-	return $form;
-}
+// function html5_search_form( $form ) { 
+// 	$form = '<section class="search">
+// 				<form role="search" method="get" id="search-form" action="' . home_url( '/' ) . '" >
+//    					<label class="screen-reader-text" for="s">' . __('',  'domain') . '</label>
+// 					<input type="search" value="' . get_search_query() . '" name="s" id="s" placeholder="&#x1F50D;"/>
+// 				</form>
+// 			</section>';
+// 	return $form;
+// }
 
-add_filter( 'get_search_form', 'html5_search_form' );
+// add_filter( 'get_search_form', 'html5_search_form' );
 
 /** 
  * Add image sizes. 
