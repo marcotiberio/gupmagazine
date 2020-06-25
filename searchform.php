11bf1cf0498431ac5d1765a072338bcce1833286
Marco <marco@defrostudio.eu>
2020-06-25 13:43:32 +0200

    searchform

--- searchform.php
+++ searchform.php
@@ -3,5 +3,4 @@
 		<span class="screen-reader-text"><?php _x( 'Search for:', 'label' )?></span>
 		<input type="search" class="search-field" placeholder="&#x1F50D;" value="<?php echo get_search_query() ?>" name="s" />
 	</label>
-	<button type="submit" class="search-submit"><i class="fa fa-search"></i></button>
 </form>
