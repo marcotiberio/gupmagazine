e4e24626386245d34d30a5afc5cf53ab92ee670d
Marco <marco@defrostudio.eu>
2020-06-29 15:59:14 +0200

    image search

--- searchform.php
+++ searchform.php
@@ -3,7 +3,7 @@
 		<span class="screen-reader-text"><?php _x( 'Search for:', 'label' )?></span>
 		<div class="search-button">
 			<input type="search" class="search-field" placeholder="" value="<?php echo get_search_query() ?>" name="s" />
-			<input type="image" src="gup-online/search.png" alt="Submit" width="10" height="10" />
+			<input type="image" src="https://gupmagazine.kinsta.cloud/wp-content/uploads/2020/06/search.png" alt="Submit" width="10" height="10" />
 		</div>
 	</label>
 </form>
