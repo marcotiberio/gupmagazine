e87d2b7c7cbe0a649cc1bc227092a6e96d5122b9
Marco <marco@defrostudio.eu>
2020-06-29 16:08:17 +0200

    dhchj

--- searchform.php
+++ searchform.php
@@ -1,8 +1,6 @@
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ) ?>">
 	<label>
 		<span class="screen-reader-text"><?php _x( 'Search for:', 'label' )?></span>
-		<div class="search-button">
-			<input type="search" class="search-field" placeholder="&#x1F50D" value="<?php echo get_search_query() ?>" name="s" />
-		</div>
+		<input type="search" class="search-field" placeholder="&#x1F50D" value="<?php echo get_search_query() ?>" name="s" />
 	</label>
 </form>
