60ec3729c225d03562e7cd6bbb9743a41e3e873e
Marco <marco@defrostudio.eu>
2020-06-29 14:25:22 +0200

    placeholder

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ) ?>">
 	<label>
 		<span class="screen-reader-text"><?php _x( 'Search for:', 'label' )?></span>
-		<input type="search" class="search-field" placeholder="assets/img/close.svg;" value="<?php echo get_search_query() ?>" name="s" />
+		<input type="search" class="search-field" placeholder="🔍" value="<?php echo get_search_query() ?>" name="s" />
 	</label>
 </form>
